4b13fb4ff41d16b2c0657d2c9e5676250cdfebf7
박상민 <psm0102psm@gmail.com>
2021-07-06 22:23:04 +0900

    Add: HackerRank Search Pairs문제

---
+++ hackerRank/Search/Pairs.js
@@ -0,0 +1,11 @@
+function solution(k, arr) {
+  let answer = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (Math.abs(arr[i] - arr[j]) === k) answer++;
+    }
+  }
+
+  return answer;
+}
